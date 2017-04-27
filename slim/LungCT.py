df05bdd1f62ecf9f14960e290794785c6618c824
Lei Wang <Lei Wang>
2017-04-27 16:41:11 -0400

    processing medical images

---
+++ slim/LungCT.py
@@ -0,0 +1,340 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import skimage, os
+from skimage.morphology import ball, disk, dilation, binary_erosion, remove_small_objects, erosion, closing, reconstruction, binary_closing
+from skimage.measure import label,regionprops, perimeter
+from skimage.morphology import binary_dilation, binary_opening
+from skimage.filters import roberts, sobel
+from skimage import measure, feature
+from skimage.segmentation import clear_border
+from skimage import data
+from scipy import ndimage as ndi
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import dicom                         # 只能读取单幅 ".dcm"的图像
+import scipy.misc
+import numpy as np
+
+import SimpleITK as sitk # 医学图像读取 
+
+#--------------------------------------------------------------
+ImgFile = "E:\\Luna16\\subset0\\1.3.6.1.4.1.14519.5.2.1.6279.6001.105756658031515062000744821260.mhd"
+inputImg =sitk.ReadImage(ImgFile)
+
+# read all of 3D CT image with extention '.mhd'
+ImgPath = "E:\\Luna16\\subset0"
+
+for fileName in os.listdir(ImgPath):
+    if file.endswith(".mhd"):
+        Img3D =sitk.ReadImage(os.path.join(ImgPath, fileName))
+        Img4D = np.stack(Img3D, axis=2) # stack 3D image to 4D image along the 'axie'
+
+
+# print(inputImg) # 打印 inputImg具有的图像信息，如 size和 spacing
+# output =sitk.DiscreteGaussianFilter(inputImg, 1.0, 5)
+# Create an image
+# image =sitk.Image(256, 256, 256,sitk.sitkInt16);
+# How about 2d?
+# twoD =sitk.Image(64, 64,sitk.sitkFloat32 )
+
+# Addressing pixels
+# image.GetPixel(0, 0, 0)
+# image.SetPixel(0, 0, 0, 1)
+# image.GetPixel(0, 0, 0)
+
+# image[0, 0, 0]  		# shorthand for Image.GetPixel(0,0,0)
+# image[0, 0, 0] = 10	# shorthand for Image.SetPixel(0,0,0,10)
+# image[0, 0, 0]
+# 
+# b =sitk.Image(image)	# Clone image
+
+#sitk.Show(image) 	# show image
+#
+# Simple smoothing
+# smooth =sitk.SmoothingRecursiveGaussian(image, 2.0)
+#sitk.Showsitk.Subtract(image, smooth))
+
+#1 # Much better
+# print("Before: ", smooth.GetPixelIDTypeAsString())
+# smooth =sitk.Cast(smooth, image.GetPixelIDValue())
+# print("After: ", smooth.GetPixelIDTypeAsString())
+#sitk.Showsitk.Subtract(image, smooth), "DiffWithGaussian")
+
+# # Extract
+# size = [ 6 4 , 64 , 1 ]
+# start = [ 6 4 , 0 , 0 ]
+#sitk.Showsitk.Extract(image, size, start), "Extracted")
+# Use pixel - wise operators
+#sitk.Show(127*image + 127sitk.BinaryErode(image), "ThinErosion")
+#
+#sitk.Hash(image + 2)
+#sitk.Hashsitk.AddConstantTo(image, 2))
+#sitk.Hash(image * 2)
+#sitk.Hashsitk.MultiplyByConstant(image, 2))
+
+## quick visualizations of masked image
+#sitk.Show(image * mask)
+#sitk.Show(0.5*image*~mask+image*mask)
+# 
+# import sys
+# if len(sys.argv) < 4:
+# 	print("Usage: SimpleGaussian <input> <sigma> <output>");
+# sys.exit(1)
+# reader =sitk.ImageFileReader()
+# reader.SetFileName(sys.argv[1])
+# image = reader.Execute()
+# pixelID = image.GetPixelIDValue()
+# gaussian =sitk.SmoothingRecursiveGaussianImageFilter()
+# gaussian.SetSigma(float(sys.argv[2]))
+# image = gaussian.Execute(image)
+# caster =sitk.CastImageFilter()
+# caster.SetOutputPixelType(pixelID)
+# image = caster.Execute(image)
+# writer =sitk.ImageFileWriter()
+# writer.SetFileName(sys.argv[3])
+# writer.Execute(image);
+
+
+
+
+# import SimpleITK as sitk
+ 
+# #read the images
+# fixed_image =  sitk.ReadImage('training_001_ct.mha', sitk.sitkFloat32)
+# moving_image = sitk.ReadImage('training_001_mr_T1.mha', sitk.sitkFloat32) 
+#  
+# #initial alignment of the two volumes
+# transform = sitk.CenteredTransformInitializer(fixed_image, moving_image, 
+# 												sitk.Euler3DTransform(), 
+# 												sitk.CenteredTransformInitializerFilter.GEOMETRY) 
+# 
+# #multi-resolution rigid registration using Mutual Information
+# registration_method = sitk.ImageRegistrationMethod()
+# registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+# registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+# registration_method.SetMetricSamplingPercentage(0.01)
+# registration_method.SetInterpolator(sitk.sitkLinear)
+# registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, 
+# 													numberOfIterations=100, 
+# 													convergenceMinimumValue=1e-6,
+# 													convergenceWindowSize=10)
+# registration_method.SetOptimizerScalesFromPhysicalShift()
+# registration_method.SetShrinkFactorsPerLevel(shrinkFactors = [4,2,1])
+# registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2,1,0])
+# registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+# registration_method.SetInitialTransform(transform)
+# registration_method.Execute(fixed_image, moving_image) 
+# 
+# sitk.WriteTransform(transform, 'ct2mrT1.tfm')
+#--------------------------------------------------------------
+
+
+
+
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+print(check_output(["ls", "../input"]).decode("utf8"))
+
+print(check_output(["ls", "../input/sample_images/"]).decode("utf8"))
+
+
+
+
+# Any results you write to the current directory are saved as output.
+lung = dicom.read_file('../input/sample_images/00cba091fa4ad62cc3200a657aeb957e/38c4ff5d36b5a6b6dc025435d62a143d.dcm')
+
+slice = lung.pixel_array
+slice[slice == -2000] = 0
+plt.imshow(slice, cmap=plt.cm.gray)
+
+
+
+def read_ct_scan(folder_name):
+        # Read the slices from the dicom file
+        slices = [dicom.read_file(folder_name + filename) for filename in os.listdir(folder_name)]
+        
+        # Sort the dicom slices in their respective order
+        slices.sort(key=lambda x: int(x.InstanceNumber))
+        
+        # Get the pixel values for all the slices
+        slices = np.stack([s.pixel_array for s in slices])
+        slices[slices == -2000] = 0
+        return slices
+
+ct_scan = read_ct_scan('../input/sample_images/00cba091fa4ad62cc3200a657aeb957e/') 
+
+
+
+def plot_ct_scan(scan):
+    f, plots = plt.subplots(int(scan.shape[0] / 20) + 1, 4, figsize=(25, 25))
+    for i in range(0, scan.shape[0], 5):
+        plots[int(i / 20), int((i % 20) / 5)].axis('off')
+        plots[int(i / 20), int((i % 20) / 5)].imshow(scan[i], cmap=plt.cm.bone) 
+
+plot_ct_scan(ct_scan)
+
+
+
+
+def get_segmented_lungs(im, plot=False):
+    
+    '''
+    This funtion segments the lungs from the given 2D slice.
+    '''
+    if plot == True:
+        f, plots = plt.subplots(8, 1, figsize=(5, 40))
+    '''
+    Step 1: Convert into a binary image. 
+    '''
+    binary = im < 604
+    if plot == True:
+        plots[0].axis('off')
+        plots[0].imshow(binary, cmap=plt.cm.bone) 
+    '''
+    Step 2: Remove the blobs connected to the border of the image.
+    '''
+    cleared = clear_border(binary)
+    if plot == True:
+        plots[1].axis('off')
+        plots[1].imshow(cleared, cmap=plt.cm.bone) 
+    '''
+    Step 3: Label the image.
+    '''
+    label_image = label(cleared)
+    if plot == True:
+        plots[2].axis('off')
+        plots[2].imshow(label_image, cmap=plt.cm.bone) 
+    '''
+    Step 4: Keep the labels with 2 largest areas.
+    '''
+    areas = [r.area for r in regionprops(label_image)]
+    areas.sort()
+    if len(areas) > 2:
+        for region in regionprops(label_image):
+            if region.area < areas[-2]:
+                for coordinates in region.coords:                
+                       label_image[coordinates[0], coordinates[1]] = 0
+    binary = label_image > 0
+    if plot == True:
+        plots[3].axis('off')
+        plots[3].imshow(binary, cmap=plt.cm.bone) 
+    '''
+    Step 5: Erosion operation with a disk of radius 2. This operation is 
+    seperate the lung nodules attached to the blood vessels.
+    '''
+    selem = disk(2)
+    binary = binary_erosion(binary, selem)
+    if plot == True:
+        plots[4].axis('off')
+        plots[4].imshow(binary, cmap=plt.cm.bone) 
+    '''
+    Step 6: Closure operation with a disk of radius 10. This operation is 
+    to keep nodules attached to the lung wall.
+    '''
+    selem = disk(10)
+    binary = binary_closing(binary, selem)
+    if plot == True:
+        plots[5].axis('off')
+        plots[5].imshow(binary, cmap=plt.cm.bone) 
+    '''
+    Step 7: Fill in the small holes inside the binary mask of lungs.
+    '''
+    edges = roberts(binary)
+    binary = ndi.binary_fill_holes(edges)
+    if plot == True:
+        plots[6].axis('off')
+        plots[6].imshow(binary, cmap=plt.cm.bone) 
+    '''
+    Step 8: Superimpose the binary mask on the input image.
+    '''
+    get_high_vals = binary == 0
+    im[get_high_vals] = 0
+    if plot == True:
+        plots[7].axis('off')
+        plots[7].imshow(im, cmap=plt.cm.bone) 
+        
+    return im
+
+
+
+
+    get_segmented_lungs(ct_scan[71], True)
+
+
+
+
+def segment_lung_from_ct_scan(ct_scan):
+	return np.asarray([get_segmented_lungs(slice) for slice in ct_scan])
+
+segmented_ct_scan = segment_lung_from_ct_scan(ct_scan)
+plot_ct_scan(segmented_ct_scan)
+
+
+segmented_ct_scan[segmented_ct_scan < 604] = 0
+plot_ct_scan(segmented_ct_scan)
+
+
+
+selem = ball(2)
+binary = binary_closing(segmented_ct_scan, selem)
+
+label_scan = label(binary)
+
+areas = [r.area for r in regionprops(label_scan)]
+areas.sort()
+
+for r in regionprops(label_scan):
+    max_x, max_y, max_z = 0, 0, 0
+    min_x, min_y, min_z = 1000, 1000, 1000
+    
+    for c in r.coords:
+        max_z = max(c[0], max_z)
+        max_y = max(c[1], max_y)
+        max_x = max(c[2], max_x)
+        
+        min_z = min(c[0], min_z)
+        min_y = min(c[1], min_y)
+        min_x = min(c[2], min_x)
+    if (min_z == max_z or min_y == max_y or min_x == max_x or r.area > areas[-3]):
+        for c in r.coords:
+            segmented_ct_scan[c[0], c[1], c[2]] = 0
+    else:
+        index = (max((max_x - min_x), (max_y - min_y), (max_z - min_z))) / (min((max_x - min_x), (max_y - min_y) , (max_z - min_z)))
+
+
+
+def plot_3d(image, threshold=-300):
+    
+    # Position the scan upright, 
+    # so the head of the patient would be at the top facing the camera
+    p = image.transpose(2,1,0)
+    p = p[:,:,::-1]
+    
+    verts, faces = measure.marching_cubes(p, threshold)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], alpha=0.1)
+    face_color = [0.5, 0.5, 1]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, p.shape[0])
+    ax.set_ylim(0, p.shape[1])
+    ax.set_zlim(0, p.shape[2])
+
+    plt.show()
+
+plot_3d(segmented_ct_scan, 604)
+
+
+
