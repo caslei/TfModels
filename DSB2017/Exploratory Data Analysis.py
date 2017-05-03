27c0a9c2cd26e85e4215b2c1b7989c82aa83c714
caslei <lei.wang.sibet@gmail.com>
2017-05-03 16:34:54 -0400

    add comments

--- DSB2017/Exploratory Data Analysis.py
+++ DSB2017/Exploratory Data Analysis.py
@@ -141,7 +141,6 @@ print('The test file has {} patients'.format(len(sample)))
 
 import dicom
 
-
 dcm = '../input/sample_images/0a38e7597ca26f9374f8ea2770ba870d/' + 
 						  '4ec5ef19b52ec06a819181e404d37038.dcm'
 print('Filename: {}'.format(dcm))
